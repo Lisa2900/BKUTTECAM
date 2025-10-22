12c08461eb17c4fcbc3c3f53b893095af1e752dc
javier-hernandez-reyes <2t0r5i7x@gmail.com>
2025-10-22 13:49:36 -0600

    Creación del modelo SolicitudesImss

--- src/controllers/nosotrosController.ts
+++ src/controllers/nosotrosController.ts
@@ -1,6 +1,6 @@
 
 import { NextFunction, Request, Response } from "express";
-import NosotrosContenido, { TipoContenido } from "../models/Nosotros";
+import NosotrosContenido, { TipoContenido } from "../models/nosotros";
 import { ValidationError, Op } from "sequelize";
 import { deleteFile } from "../middleware/uploadMiddleware";
 import path from "path";
